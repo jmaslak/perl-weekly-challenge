957f4d09e7fe70178f9fe7743d99355fbc46c13a
Joelle Maslak <jmaslak@antelope.net>
2019-03-30 09:45:41 -0600

    Add comments to problem 1

--- week1/1.pl
+++ week1/1.pl
@@ -3,6 +3,16 @@ use v5.26;
 use strict;
 use warnings;
 
+# Challenge:
+#   Write a script to replace the character ‘e’ with ‘E’ in the
+#   string ‘Perl Weekly Challenge’. Also print the number of times
+#   the character ‘e’ is found in the string.
+
+# Methodology:
+#   m/e/g will return all the matches in array context.
+#   s/e/E/gr uses the "r" modifier to non-destructively modify the
+#            original string.
+
 my $orig      = 'Perl Weekly Challenge';
 my (@matches) = $orig =~ m/e/g;
 my $new       = $orig =~ s/e/E/gr;
